a036635f1b765d4dc60af369c4906464a8ae6913
Andrey Pakhomenkov <appmailbox@mail.ru>
2012-01-04 21:58:59 +0400

    120104 2158 developing

--- main.pas
+++ main.pas
@@ -20,13 +20,29 @@ type
     lbTime: TLabel;
     lbDate: TLabel;
     Panel1: TPanel;
+    ClockTimer: TTimer;
+    TrayIcon1: TTrayIcon;
+    procedure ClockTimerTimer(Sender: TObject);
     procedure FormActivate(Sender: TObject);
+    procedure TrayIcon1Click(Sender: TObject);
+    procedure TrayIcon1DblClick(Sender: TObject);
   private
     { private declarations }
     msLocaleFolder : String;
     //***** Clock
-    masMonths : array[1..ciMonthCount] of String;
+    masMonths   : array[1..ciMonthCount] of String;
     masWeekDays : array[1..ciWeekDayCount] of String;
+    msClockTimeHint : String;
+    msClockDateHint : String;
+
+    mwClockYear,
+    mwClockMonth,
+    mwClockDay,
+    mwClockWeekDay,
+    mwClockHours,
+    mwClockMinutes,
+    mwClockSeconds,
+    mwClockMSeconds : Word;
   public
     { public declarations }
 
@@ -34,6 +50,7 @@ type
     function ReadConfig : Boolean;
     function ReadLocale : Boolean;
 
+    procedure AskSystemDateAndTime;
     procedure DisplayDate;
     procedure DisplayTime;
   end;
@@ -51,8 +68,72 @@ begin
   OnActivate:=Nil;
   ReadConfig; //
   ReadLocale; //
+  AskSystemDateAndTime;
   DisplayTime;
   DisplayDate;
+end;
+
+
+procedure TfmMain.TrayIcon1Click(Sender: TObject);
+begin
+
+  TrayIcon1.BalloonHint:=msClockDateHint+LF+msClockTimeHint;
+  TrayIcon1.ShowBalloonHint;
+end;
+
+
+procedure TfmMain.TrayIcon1DblClick(Sender: TObject);
+begin
+
+  if fmMain.Visible then
+    fmMain.Hide
+  else
+    fmMain.Show;
+end;
+
+
+procedure TfmMain.ClockTimerTimer(Sender: TObject);
+var   ldtNow     : TDateTime;
+      lsTrayHint : String;
+begin
+
+  //***** Минуту еще не натикало?
+  if mwClockSeconds<ciMaxSecond then begin
+
+    //DecodeTime(Time,m_wClockHours,m_wClockMinutes,m_wClockSeconds,m_wClockMSeconds);
+    inc(mwClockSeconds);
+    fmMain.Update;
+  end else begin
+
+    //***** Уже натикало
+    mwClockSeconds:=0;
+
+
+    //***** Час еще не натикало?
+    if mwClockMinutes<ciMaxMinute then begin
+
+      //inc(mwClockMinutes)
+      AskSystemDateAndTime;
+      DisplayTime;
+      DisplayDate;
+
+    end else begin
+
+      //***** Уже натикало
+      mwClockMinutes:=0;
+
+      //***** Сутки не натикали еще?
+      if mwClockHours<ciMaxHour then begin
+
+        inc(mwClockHours);
+      end else begin
+
+        //***** Уже натикали
+        mwClockHours:=0;
+      end;
+
+    end;
+  end;
 end;
 
 
@@ -87,7 +168,7 @@ begin
 
     //***** Названия дней недели
     for liIdx:=1 to ciWeekDayCount do begin
-      masMonths[liIdx]:=IniReadString('weekdays','weekday'+IntToStr(liIdx));
+      masWeekDays[liIdx]:=IniReadString('weekdays','weekday'+IntToStr(liIdx));
     end;
 
     IniClose;
@@ -96,30 +177,36 @@ begin
 end;
 
 
-procedure TfmMain.DisplayDate;
+procedure TfmMain.AskSystemDateAndTime;
 var ldtNow : TDateTime;
 begin
+
   ldtNow:=Now;
-  lbDate.Caption:=AlignRight(IntToStr(DayOf(ldtNow)),2,'0')+' '+
-                  masMonths[MonthOf(ldtNow)]+' '+
-                  IntToStr(YearOf(ldtNow));
+  DecodeTime(ldtNow,mwClockHours,mwClockMinutes,mwClockSeconds,mwClockMSeconds);
+  DecodeDate(ldtNow,mwClockYear,mwClockMonth,mwClockDay);
+  mwClockWeekDay:=DayOfTheWeek(ldtNow);
+end;
+
+
+procedure TfmMain.DisplayDate;
+begin
+
+  msClockDateHint:=AlignRight(IntToStr(mwClockDay),2,'0')+' '+
+                  masMonths[mwClockMonth]+' '+
+                  IntToStr(mwClockYear);
+  lbDate.Caption:=msClockDateHint;
 end;
 
 
 procedure TfmMain.DisplayTime;
-var lwClockHours,
-    lwClockMinutes,
-    lwClockSeconds,
-    lwClockMSeconds : Word;
-    ldtNow          : TDateTime;
 begin
 
-  ldtNow:=Now;
-  DecodeTime(ldtNow,lwClockHours,lwClockMinutes,lwClockSeconds,lwClockMSeconds);
-  lbTime.Caption:=IntToStr(lwClockHours)+':'+
-                   AlignRight(IntToStr(lwClockMinutes),2,'0')+':'+
-                   AlignRight(IntToStr(lwClockSeconds),2,'0');
+  msClockTimeHint:=IntToStr(mwClockHours)+':'+
+                   AlignRight(IntToStr(mwClockMinutes),2,'0')+':'+
+                   AlignRight(IntToStr(mwClockSeconds),2,'0');
+  lbTime.Caption:=msClockTimeHint;
 end;
+
 
 end.
 
